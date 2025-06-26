de2cd839500b0f0a199d343eaa191526c2710e2b
Fedora de Flandre <s287754@polito.studenti.it>
2025-06-26 19:05:08 +0200

    Aggiorna .gitignore

--- Debug/CMakeFiles/GeodesicPolyhedra.dir/src/DataGenerator.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/GeodesicPolyhedra.dir/src/DataGenerator.cpp.o: \
- /home/appuser/Data/Project/ProgettoPCS2025/src/DataGenerator.cpp \
- /usr/include/stdc-predef.h
--- Debug/CMakeFiles/GeodesicPolyhedra_TEST.dir/src/DataGenerator.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/GeodesicPolyhedra_TEST.dir/src/DataGenerator.cpp.o: \
- /home/appuser/Data/Project/ProgettoPCS2025/src/DataGenerator.cpp \
- /usr/include/stdc-predef.h
